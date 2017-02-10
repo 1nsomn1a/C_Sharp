6483b3e4f89b1cdeaf997984f8476aa707e6ad77
Stanislav <stas.starodubtsev@gmail.com>
2017-02-10 14:29:01 +0300

    add Properties

---
+++ Properties/Properties/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class TV
+    {
+        private int volume;
+        private int number;
+
+        public TV(int number) //Constructor
+        {
+            this.number = number;
+        }
+
+        //public int Number { get; set; }   //cant use
+
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+        } 
+
+        //public int Volume { get; set; }
+
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value > 100)
+                {
+                    volume = 100;
+                }
+                else if (value < 0)
+                {
+                    volume = 0;
+                }
+                else
+                    volume = value;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TV myTV = new TV(234);
+
+            myTV.Volume = 101;
+            Console.WriteLine(myTV.Volume + " " + myTV.Number );
+        }
+    }
+}
