700847a1ce8dd50c0b94f8d23a89bc01bd65b5ed
Stanislav <stas.starodubtsev@gmail.com>
2017-02-21 22:48:31 +0300

    add OverloadMethods

---
+++ OverloadedMethod/OverloadedMethod/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedMethod
+{
+    class Example
+    {
+        public static void GetNames(List<string> names)
+        {
+            int tmp = names.Count;
+            foreach (var n in names)
+            {
+                if (tmp == 1)
+                    Console.Write(n);
+                else
+                {
+                    Console.Write(n + ", ");
+                    tmp--;
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void GetNames(List<string> names, string s)
+        {
+            int tmp = names.Count;
+            foreach (var n in names)
+            {
+                if (tmp == 1)
+                    Console.Write(n);
+                else
+                { 
+                    Console.Write(n + $" {s} ");
+                    tmp--;
+                }
+            }
+
+            Console.WriteLine();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { "Kolya", "Johny", "Wolfgang", "Tomas", "Roberto" };
+
+            Example.GetNames(names);
+            Example.GetNames(names, "|");
+            Console.ReadKey();
+
+        }
+    }
+}
